944f58db769ddb65c4f62874c7f1f208d6df2b1f
shang <shangxz@umich.edu>
2019-02-24 11:08:16 -0500

    fixed quick bug

--- server.js
+++ server.js
@@ -195,7 +195,7 @@ money.post('/', async function (req, res) {
     console.log(customer);
     var final_customer = customer[0][0].balance;
     data = {"merchant" : final_parking};
-    data["License " + plate] = plate;
+    data["License " + plate] = customer;
 
     res.send(data);
 });
